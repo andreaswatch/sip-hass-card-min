bdd5014097913f73b3240d37f7d073ff3f814b8a
Andreas <strauss.mobil@googlemail.com>
2025-04-20 13:16:43 +0200

    Update sipjs-card.ts
    Add AEC

--- src/sipjs-card.ts
+++ src/sipjs-card.ts
@@ -765,7 +765,11 @@ class SipJsCard extends LitElement {
         this.sipPhone = new UA(configuration);
 
         this.sipCallOptions = {
-            mediaConstraints: { audio: true, video: this.config.video },
+            mediaConstraints: { audio: {
+      echoCancellation: true, // explicitly enable
+      noiseSuppression: true, // recommended
+      autoGainControl: true   // recommended
+    }, video: this.config.video },
             rtcOfferConstraints: { offerToReceiveAudio: true, offerToReceiveVideo: this.config.video },
             pcConfig: this.config.iceConfig // we just use the config that directly comes from the YAML config in the YAML card config.
             /* EXAMPLE config
